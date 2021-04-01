46390a03b6cc967a086e690ddefb28e215380bb3
Lee Jordan <duracell80@users.noreply.github.com>
2021-03-31 23:59:22 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # List Internet Radio Streams to DLNA Directory with Rygel
 
 sudo apt update
-sudo apt-get install -y pavucontrol paprefs gupnp-tools gstreamer1.0-tools rygel rygel-gst-launch rygel-playbin rygel-tracker
+sudo apt-get install -y gupnp-tools gstreamer1.0-tools rygel rygel-gst-launch rygel-playbin rygel-tracker
 
 touch ~/.config/rygel.conf
 chmod a+x start.sh
