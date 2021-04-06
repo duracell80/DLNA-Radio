bbcbb76ca0a4f1a634f683e431b1c0985e04bab2
Lee <lee.jordan@gmail.com>
2021-04-05 23:53:23 -0500

    Adding touch

--- install.sh
+++ install.sh
@@ -5,8 +5,9 @@ sudo apt update
 sudo apt-get install -y gupnp-tools gstreamer1.0-tools rygel rygel-gst-launch rygel-playbin rygel-tracker
 
 touch ~/.config/rygel.conf
-touch ./got_youtube.txt
-chmod a+x start-linux.sh
-chmod a+x got_youtube.txt
+touch ~/DLNA-Radio/got_youtube.txt
+chmod a+x ~/DLNA-Radio/got_youtube.txt
+chmod a+x start.sh
 
-python3 main-linux.py
+
+python3 main.py
