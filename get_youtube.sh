cddcd37067905f5623188a333a19357389c65a9b
Lee Jordan <duracell80@users.noreply.github.com>
2022-01-27 16:03:14 -0600

    Update get_youtube.sh

--- get_youtube.sh
+++ get_youtube.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Use youtube-dl to get streamable audio URL for insertion into rygel.conf
 
-youtube-dl --format '140[ext=m4a][protocol=https]' --get-url $1 > /home/lee/DLNA-Radio/got_youtube.txt
+youtube-dl --format '140[ext=m4a][protocol=https]' --get-url $1 > /home/pi/DLNA-Radio/got_youtube.txt
